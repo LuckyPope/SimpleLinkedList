b1d87d30461a3a1f7c29ab7d2ed13395b4865bcf
Daniil <zhilyaev_18@mail.ru>
2022-03-25 21:19:43 +0300

    process

---
+++ src/ru/vsu/cs/zhilyaev/Main.java
@@ -0,0 +1,24 @@
+package ru.vsu.cs.zhilyaev;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        list.addFirst(1);
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(2);
+        list.addFirst(1);
+        list.addFirst(1);
+        System.out.println("Начальный список:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print((i > 0 ? ", " : "") + list.get(i));
+        }
+        System.out.println();
+        list.removeRepeats();
+        System.out.println("Изменненный список:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print((i > 0 ? ", " : "") + list.get(i));
+        }
+    }
+}
